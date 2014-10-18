bf4474f3055e661fd059f25bd74ee4d70f51af32
David Garcia Miguel <noxwings@gmail.com>
2014-10-18 16:51:06 +0200

    Unused code removed

--- HydraEmulation.py
+++ HydraEmulation.py
@@ -16,17 +16,12 @@ def setup(index):
 def updateSmoothTracking():
 	hydra[0].x         = filters.simple(-psmove[0].position.x, Smoothing) * PositionScale
 	hydra[0].y         = filters.simple( psmove[0].position.y, Smoothing) * PositionScale
-	hydra[0].z         = filters.simple(-psmove[0].position.z, Smoothing) * PositionScale
+	hydra[0].z         = filters.simple(-psmove[0].position.z, DepthSmooth) * PositionScale
 	hydra[1].x         = filters.simple(-psmove[1].position.x, Smoothing) * PositionScale
 	hydra[1].y         = filters.simple( psmove[1].position.y, Smoothing) * PositionScale
-	hydra[1].z         = filters.simple(-psmove[1].position.z, Smoothing) * PositionScale
-
+	hydra[1].z         = filters.simple(-psmove[1].position.z, DepthSmooth) * PositionScale
 	
 def updateHydra(i):
-	#hydra[i].x         = -psmove[i].position.x * PositionScale
-	#hydra[i].y         =  psmove[i].position.y * PositionScale
-	#hydra[i].z         = -psmove[i].position.z * PositionScale
-	
 	hydra[i].yaw       =  psmove[i].pitch
 	hydra[i].pitch     = -psmove[i].roll
 	hydra[i].roll      =  psmove[i].yaw
@@ -41,14 +36,14 @@ def updateHydra(i):
 	hydra[i].start     = xbox360[i].down
 	hydra[i].isDocked  = xbox360[i].up
 
-	hydra[i].trigger   = xbox360[i].leftTrigger * TriggerScale 
+	hydra[i].trigger   = (xbox360[i].leftTrigger + psmove[i].trigger)* TriggerScale 
 	hydra[i].joyx      = xbox360[i].leftStickX  * AxisScale
 	hydra[i].joyy      = xbox360[i].leftStickY  * AxisScale
 
-def update(index):
-	if psmove[index].getButtonDown( PSMoveButton.Move ):
-		setupMove(index)
-	updateHydra(index)
+def update(i):
+	if psmove[i].getButtonDown(PSMoveButton.Move) or xbox360[i].down:
+		setupMove(i)
+	updateHydra(i)
 		
 if starting:
 	Left = 0
@@ -56,6 +51,7 @@ if starting:
 	AxisScale = TriggerScale = 1
 	PositionScale = 50
 	Smoothing = 0.25
+	DepthSmooth = 0.35
 	setup(Left)
 	setup(Right)
 
